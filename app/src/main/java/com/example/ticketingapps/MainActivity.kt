917cd69eb99faf54d73ff2ef796bd469e440e8ad
zakymzn <mamurzakynurrokhman@gmail.com>
2025-05-22 15:09:47 +0700

    result

--- app/src/main/java/com/example/ticketingapps/MainActivity.kt
+++ app/src/main/java/com/example/ticketingapps/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.ticketingapps
 
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,22 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        supportActionBar?.hide()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val seatView = findViewById<SeatsView>(R.id.seatsView)
+        val button = findViewById<Button>(R.id.finishButton)
+
+        button.setOnClickListener {
+            seatView.seat?.let {
+                Toast.makeText(this, "Kursi Anda nomor ${it.name}", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Silakan pilih kursi terlebih dahulu", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 }
